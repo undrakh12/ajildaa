4929cf5b47c1433f85ec7387f237c8c9e91fdfa7
Nasanjargal <nasanjargal@batu.digital>
2018-02-27 22:21:55 +0800

    index bolon result 2in dund transition orulav

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,143 +19,147 @@
 </head>
 
 <body>
-	<header class="header" uk-sticky="top: 0">
-		<nav class="main-navbar uk-navbar-container" uk-navbar>
-			<div class="uk-navbar-left">
-				<ul class="uk-navbar-nav">
-					<li class="uk-active">
-						<a>Ажил хайх</a>
-					</li>
-				</ul>
+	<main id="barba-wrapper">
+      <div class="barba-container" data-prev="/" data-next="/result">
+			<header class="header" uk-sticky="top: 0">
+				<nav class="main-navbar uk-navbar-container" uk-navbar>
+					<div class="uk-navbar-left">
+						<ul class="uk-navbar-nav">
+							<li class="uk-active">
+								<a href="/" class="prev">Ажил хайх</a>
+							</li>
+						</ul>
 
-			</div>
-		</nav>
-	</header>
-
-	<main class="body">
-		<div class="uk-container uk-container-center">
-			<div class="uk-grid-small uk-grid-match uk-text-center uk-margin-top" uk-grid>
-				<div class="uk-width-1-1">
-					<div class="head-section ">
-						<img src="images/logo.png" class="uk-responsive-width logo" alt="" uk-responsive>
 					</div>
+				</nav>
+			</header>
 
-					<div class="search-section uk-margin-top">
-						<form action="/result" id="form">
-							<div class="first-search-section">
-								<h6 class="uk-margin-small-bottom h6-title uk-margin-left uk-text-left">
-									<strong>ЮУ</strong> ажлын нэр, үг, эсвэл компани</h6>
-								<div class="uk-margin uk-margin-remove-top">
-									<div class="uk-inline search-inline">
-										<a class="uk-form-icon uk-form-icon-flip" href="#" uk-icon="icon: search"></a>
-										<input class="search-input uk-input" type="text" id="first" data-dependency="second">
+			<section class="body">
+				<div class="uk-container uk-container-center">
+					<div class="uk-grid-small uk-grid-match uk-text-center uk-margin-top" uk-grid>
+						<div class="uk-width-1-1">
+							<div class="head-section ">
+								<img src="images/logo.png" class="uk-responsive-width logo" alt="" uk-responsive>
+							</div>
+
+							<div class="search-section uk-margin-top">
+								<!-- <form action="/result" id="form"> -->
+									<div class="first-search-section">
+										<h6 class="uk-margin-small-bottom h6-title uk-margin-left uk-text-left">
+											<strong>ЮУ</strong> ажлын нэр, үг, эсвэл компани</h6>
+										<div class="uk-margin uk-margin-remove-top">
+											<div class="uk-inline search-inline">
+												<a class="uk-form-icon uk-form-icon-flip" href="#" uk-icon="icon: search"></a>
+												<input class="search-input uk-input" type="text" id="first" data-dependency="second">
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
 
-							<div class="second-search-section">
-								<h6 class="uk-margin-small-bottom uk-margin-left uk-text-left">
-									<strong>ХААНА</strong> хот, дүүрэг, эсвэл зип код</h6>
-								<div class="uk-margin uk-margin-remove-top">
-									<div class="uk-inline search-inline">
-										<a class="uk-form-icon uk-form-icon-flip" href="#" uk-icon="icon: location"></a>
-										<input class="search-input uk-input" type="text" id="second" data-dependency="third">
+									<div class="second-search-section">
+										<h6 class="uk-margin-small-bottom uk-margin-left uk-text-left">
+											<strong>ХААНА</strong> хот, дүүрэг, эсвэл зип код</h6>
+										<div class="uk-margin uk-margin-remove-top">
+											<div class="uk-inline search-inline">
+												<a class="uk-form-icon uk-form-icon-flip" href="#" uk-icon="icon: location"></a>
+												<input class="search-input uk-input" type="text" id="second" data-dependency="third">
+											</div>
+										</div>
 									</div>
-								</div>
+
+									<div class="third-section uk-margin-medium-top">
+
+										<a href="/result" class="uk-button uk-button-primary btn next" id="third">Цагийн ажил хайх</a>
+									</div>
+								<!-- </form> -->
 							</div>
 
-							<div class="third-section uk-margin-medium-top">
-
-								<button class="uk-button uk-button-primary btn" id="third">Цагийн ажил хайх</button>
-							</div>
-						</form>
+						</div>
 					</div>
-
 				</div>
-			</div>
+			</section>
+
+			<footer class="footer-section uk-margin-medium-top">
+				<div class="rec-jobs">
+					<div class="uk-grid">
+						<div class="uk-width-1-1 first-table">
+							<ul class="uk-list border-top">
+								<li class="uk-padding uk-padding-small">
+									<div class="uk-clearfix">
+										<div class="uk-float-left">
+											<a href="">
+												<h5>
+													<span class="uk-margin-small-right" uk-icon="settings"></span>Санал Болгож Буй Ажил</h5>
+											</a>
+										</div>
+										<div class="uk-float-right">
+											<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
+										</div>
+
+									</div>
+								</li>
+								<li class="uk-padding uk-padding-small uk-padding-remove-top">
+									<div class="uk-clearfix">
+										<div class="uk-float-left">
+											<a href="/viewjob">
+												<h5>
+													<span class="uk-margin-small-right" uk-icon="nut"></span>Миний Анкет</h5>
+											</a>
+										</div>
+										<div class="uk-float-right">
+											<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
+										</div>
+
+									</div>
+								</li>
+								<li class="uk-padding uk-padding-small uk-padding-remove-top">
+									<div class="uk-clearfix">
+										<div class="uk-float-left">
+											@guest
+											<a href="/login">
+												<h5>
+													<span class="uk-margin-small-right" uk-icon="user"></span>Нэвтрэх
+												</h5>
+											</a>
+											@else
+											<a href="/profile">
+												<h5>
+													<span class="uk-margin-small-right" uk-icon="user"></span>
+													{{ Auth::user()->email }}
+												</h5>
+											</a>
+											@endguest
+										</div>
+										<div class="uk-float-right">
+											<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
+										</div>
+									</div>
+								</li>
+								<li class="uk-padding uk-padding-small uk-padding-remove-top">
+									<div class="uk-clearfix post-link">
+										<div class="uk-float-left">
+											<a href="/post-job">
+												<h5>
+													<span class="uk-margin-small-right" uk-icon="bell"></span>Ажил Нийтлэх</h5>
+											</a>
+										</div>
+										<div class="uk-float-right">
+											<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
+										</div>
+
+									</div>
+								</li>
+							</ul>
+
+							<div class="footer-section uk-padding uk-padding-small">
+								<h6>©2018 Ажилдаа - Бүх эрх хуулиар хамгаалагдсан</h6>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</footer>
 		</div>
 	</main>
-
-	<footer class="footer-section uk-margin-medium-top">
-		<div class="rec-jobs">
-			<div class="uk-grid">
-				<div class="uk-width-1-1 first-table">
-					<ul class="uk-list border-top">
-						<li class="uk-padding uk-padding-small">
-							<div class="uk-clearfix">
-								<div class="uk-float-left">
-									<a href="">
-										<h5>
-											<span class="uk-margin-small-right" uk-icon="settings"></span>Санал Болгож Буй Ажил</h5>
-									</a>
-								</div>
-								<div class="uk-float-right">
-									<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
-								</div>
-
-							</div>
-						</li>
-						<li class="uk-padding uk-padding-small uk-padding-remove-top">
-							<div class="uk-clearfix">
-								<div class="uk-float-left">
-									<a href="/viewjob">
-										<h5>
-											<span class="uk-margin-small-right" uk-icon="nut"></span>Миний Анкет</h5>
-									</a>
-								</div>
-								<div class="uk-float-right">
-									<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
-								</div>
-
-							</div>
-						</li>
-						<li class="uk-padding uk-padding-small uk-padding-remove-top">
-							<div class="uk-clearfix">
-								<div class="uk-float-left">
-									@guest
-									<a href="/login">
-										<h5>
-											<span class="uk-margin-small-right" uk-icon="user"></span>Нэвтрэх
-										</h5>
-									</a>
-									@else
-									<a href="/profile">
-										<h5>
-											<span class="uk-margin-small-right" uk-icon="user"></span>
-											{{ Auth::user()->email }}
-										</h5>
-									</a>
-									@endguest
-								</div>
-								<div class="uk-float-right">
-									<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
-								</div>
-							</div>
-						</li>
-						<li class="uk-padding uk-padding-small uk-padding-remove-top">
-							<div class="uk-clearfix post-link">
-								<div class="uk-float-left">
-									<a href="/post-job">
-										<h5>
-											<span class="uk-margin-small-right" uk-icon="bell"></span>Ажил Нийтлэх</h5>
-									</a>
-								</div>
-								<div class="uk-float-right">
-									<span class="uk-margin-small-right" uk-icon="arrow-right"></span>
-								</div>
-
-							</div>
-						</li>
-					</ul>
-
-					<div class="footer-section uk-padding uk-padding-small">
-						<h6>©2018 Ажилдаа - Бүх эрх хуулиар хамгаалагдсан</h6>
-					</div>
-
-				</div>
-			</div>
-		</div>
-	</footer>
 
 	<!-- JS -->
 	<script src="js/uikit.min.js"></script>
@@ -163,12 +167,6 @@
 
 	<script src="js/app.js" async></script>
 
-	<!-- <script>
-		document.getElementsByClassName('post-link')[0]
-			.addEventListener('click', function (event) {
-				window.location.href = "post-job.html";
-			});
-	</script> -->
 	<script>
 		(function ($) {
 			$.fn.enterAsTab = function (options) {
@@ -205,6 +203,11 @@
 
 		$("#form").enterAsTab({ 'allowSubmit': true });
 	</script>
+
+	<!-- barba.js -->
+	<script src="js/barba.js"></script>
+    <script src="js/nextprev.js"></script>
+	<script src="js/TweenMax.min.js"></script>
 </body>
 
 </html>
