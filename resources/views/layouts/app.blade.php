a613f504eae96d35fe0ede96d97f44c8b89f6b95
mbadrala <a.badrala@gmail.com>
2018-03-04 16:36:20 +0800

    delted barba.js , jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,60 +15,14 @@
 
     <!-- manifest -->
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
-
-    <!-- jQuery -->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
 </head>
 <body>
 
     @yield('content')
-    
-    <!-- get android next button with jquery -->
-    <script>
-        (function ($) {
-            $.fn.enterAsTab = function (options) {
-                var settings = $.extend({
-                    'allowSubmit': false
-                }, options);
-                $(this).find('input, button').live("keydown", { localSettings: settings }, function (event) {
-                    if (settings.allowSubmit) {
-                        var type = $(this).attr("type");
-                        if (type == "submit") {
-                            return true;
-                        }
-                    }
-                    if (event.keyCode == 13) {
-                        var inputs = $(this).parents("form").eq(0).find(":input:visible:not(:disabled):not([readonly])");
-                        var idx = inputs.index(this);
-                        if (idx == inputs.length - 1) {
-                            idx = -1;
-                        } else {
-                            inputs[idx + 1].focus(); // handles submit buttons
-                        }
-                        try {
-                            inputs[idx + 1].select();
-                        }
-                        catch (err) {
-
-                        }
-                        return false;
-                    }
-                });
-                return this;
-            };
-        })(jQuery);
-
-        $("#form").enterAsTab({ 'allowSubmit': true });
-    </script>
 
     <!-- main JS -->
     <script src="{{ asset('js/uikit.min.js') }}"></script>
     <script src="{{ asset('js/uikit-icons.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" async></script>
-    
-    <!-- barba.js -->
-    <script src="{{ asset('js/barba.js') }}"></script>
-    <script src="{{ asset('js/nextprev.js') }}"></script>
-    <script src="{{ asset('js/TweenMax.min.js') }}"></script>
 </body>
 </html>
