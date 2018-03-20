0dfcd7e1c75e6e80c578591e9f1cd898741e61d4
nasanjarga <nasanjargal@batu.digital>
2018-03-20 15:39:03 +0800

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="theme-color" content="#000" />
+    <meta name="theme-color" content="#ffffff" />
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 
     <!-- CSS -->
